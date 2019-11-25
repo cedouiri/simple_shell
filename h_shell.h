c38ca6b8c79b68253b191af97a5e8f003a33fc7b
cedouiri <cedouiri@gmail.com>
2019-11-25 04:27:10 +0000

    declare func in header file

--- h_shell.h
+++ h_shell.h
@@ -4,5 +4,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-
+#include <sys/wait.h>
+#include <stdarg.h>
+#include <signal.h>
+char **str_tow(char *str);
+void cpString(int e, char *s, char **wb);
+void GetWord(char **w, char *str);
+int WordCount(char *str);
 #endif
