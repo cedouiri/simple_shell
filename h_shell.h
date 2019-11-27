6a4ef12d64241e2002d6078f28a67bfa09435ca7
cedouiri <cedouiri@gmail.com>
2019-11-27 02:17:35 +0000

    add func

--- h_shell.h
+++ h_shell.h
@@ -13,5 +13,9 @@ char **str_tow(char *str);
 void cpString(int e, char *s, char **wb);
 void GetWord(char **w, char *str);
 int WordCount(char *str);
+int _strlen(char *s);
+int _strcmp(char *s1, char *s2);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_getenv(const char *name);
 
 #endif
