414506b1d3879e6967363b9dd04432f9c2d53881
cedouiri <cedouiri@gmail.com>
2019-11-27 17:20:11 +0000

    add some condition

--- s_shell.c
+++ s_shell.c
@@ -11,32 +11,49 @@ int main(int ac, char *av[], char **e)
 {
 	char *p = NULL;
 	char **str = NULL;
-	int stat, length;
+	int stat, length, n = 0;
 	size_t size = 0;
 	pid_t ppid;
 	(void)ac;
 
-	while (1)
+	if (ac > 1)
 	{
-		ppid = fork();
-		if (!ppid)
+		if (execve(av[1], av + 1, NULL) == -1)
 		{
-			printf("Shell$: ");
-			length = getline(&p, &size, stdin);
-			if (length == -1)
-			{
-				dprintf(2, "Exiting\n");
-				free(str);
-				exit(-1);
-			}
+			perror("./shell");
+			exit(0);
+		}
+	}
+	while (!n)
+	{
+		if (isatty(fileno(stdin)))
+			write(2, "simple-shell$", 6);
+		ppid = fork();
+		if (ppid == 0)
+		{
+			if ((length = getline(&p, &size, stdin)) == -1)
+				n = 1;
 			p[length - 1] = '\0';
-			str = str_tow(p);
+			if (*p == '\0' || (!(str = str_tow(p))))
+			{
+				free(p);
+				exit(0);
+			}
 			_path(str, e);
+			if (str == NULL)
+				perror("Error: string is NULL\n");
 			if (execve(str[0], str, NULL) == -1)
-				perror("Error\n");
+			{
+				perror("./shell\n");
+				exit(0);
+			}
 		}
 		else
+		{
 			wait(&stat);
+			if (stat !=0 || n)
+				break;
+		}
 	}
 	printf("Ending \n");
 	return (0);
